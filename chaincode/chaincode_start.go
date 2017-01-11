0a74c34ac139255206f3fa1c761ed677c6734b08
Shourie Ganguly <shourie.dbz2007@gmail.com>
2017-01-11 16:35:20 +0530

    Add files via upload

--- chaincode/chaincode_start.go
+++ chaincode/chaincode_start.go
@@ -40,8 +40,8 @@ func main() {
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 	 
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2")
 	}
 
 	err := stub.PutState("aValue",[]byte(args[0]))
